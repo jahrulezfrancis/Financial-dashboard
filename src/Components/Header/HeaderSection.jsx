b81fefcbb2c9c04d55d93fff55c95d7efa94ba6b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-02 00:06:58 +0100

    updated header section

--- src/Components/Header/HeaderSection.jsx
+++ src/Components/Header/HeaderSection.jsx
@@ -1,10 +1,26 @@
-import { Box, Stack, Typography } from "@mui/material"
+import { Box, Button, Stack, Typography, IconButton } from "@mui/material"
+import AddIcon from '@mui/icons-material/Add';
+import { ArrowDropDown } from "@mui/icons-material";
+import { RiFilterLine } from "react-icons/ri"
 
 export default function HeaderIntro() {
     return (
         <Box>
-            <Stack>
-                <Typography>Overview</Typography>
+            <Stack direction='row' flexWrap='wrap' gap={1}>
+                <Box>
+                    <Typography variant="h5">Overview</Typography>
+                    <Button variant="outlined" sx={{ color: "black" }} startIcon={<AddIcon />}>
+                        Add Product
+                    </Button>
+                </Box>
+                <Box>
+                    <Button endIcon={<ArrowDropDown />}>
+                        Popular
+                    </Button>
+                    <IconButton>
+                        <RiFilterLine />
+                    </IconButton>
+                </Box>
             </Stack>
         </Box>
 
