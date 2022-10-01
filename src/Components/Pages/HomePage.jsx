c7670f20b4a7846838e746ea6ebd4fb2d8013541
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-01 23:21:55 +0100

    Added the header intro component

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/system";
+import HeaderIntro from "../Header/HeaderSection";
 import Navigation from "../Header/Navigation";
 
-export function Homepage(){
-    return(
-        <Navigation />
+export function Homepage() {
+    return (
+        <Box>
+            <Navigation />
+            <HeaderIntro />
+        </Box>
     )
 }
