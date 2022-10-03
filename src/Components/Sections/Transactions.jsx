d515bae39f82d6b09bc06428263d005e1e4b8579
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-03 07:12:47 +0100

    updated the transaction details page

--- src/Components/Sections/Transactions.jsx
+++ src/Components/Sections/Transactions.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, ListItemIcon, Typography } from "@mui/material";
+import { Box, Grid, Button, ListItemIcon, Typography, } from "@mui/material";
 import * as React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -7,7 +7,7 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
-import { Coffee, Store } from "@mui/icons-material";
+import { ArrowLeft, ArrowRight, Coffee, Store } from "@mui/icons-material";
 import MasterCard from "../Images/master-card.png"
 import Visa1 from "../Images/visa-red.png"
 import Visa2 from "../Images/visa-blue.png"
@@ -46,26 +46,51 @@ export function CardsTemp(props) {
     );
 }
 
+const PriceText = (props) => {
+    return (
+        <Grid alignItems='' p='1em'>
+            <Typography fontSize='2em' fontWeight='bold' color='white'>
+                {props.priceCount}
+            </Typography>
+        </Grid>
+    )
+}
 
+const LeftRightButton = () => {
+    return (
+        <Grid>
+            <Button startIcon={<ArrowLeft />} sx={{ color: 'white' }} />
+            <Button endIcon={<ArrowRight />} sx={{ color: 'white' }} />
+        </Grid>
+    )
+}
 
 export function TransactionsTab() {
     return (
-        <Box>
-            <Grid container justifyContent='space-evenly'>
-                <TransactionsTemp primaryText='Apple Store' ListIcon={<Store />} date='July 20, 2014' />
-                <CardsTemp primaryText='*4300' ListIcon={Visa1} cardType='Credit Card'/>
+        <Box width='32rem'>
+            <Grid container justifyContent='space-between' alignContent='center'>
+                <Typography color='white'>Last transaction</Typography>
+                <LeftRightButton />
             </Grid>
-            <Grid container>
+            <Grid container justifyContent='space-between' alignContent='center'>
+                <TransactionsTemp primaryText='Apple Store' ListIcon={<Store />} date='July 20, 2014' />
+                <CardsTemp primaryText='*4300' ListIcon={Visa1} cardType='Credit Card' />
+                <PriceText priceCount='$4444' />
+            </Grid>
+            <Grid container justifyContent='space-between' alignContent='center'>
                 <TransactionsTemp primaryText='Starbucks Cafe' ListIcon={<Coffee />} date='July 20, 2014' />
                 <CardsTemp primaryText='Spar' ListIcon={Visa2} cardType='Credit Card' />
+                <PriceText priceCount='$4444' />
             </Grid>
-            <Grid container>
+            <Grid container justifyContent='space-between' alignContent='center'>
                 <TransactionsTemp primaryText='Pharmacy' ListIcon={<MedicalServicesIcon />} date='July 20, 2014' />
                 <CardsTemp primaryText='Spar' ListIcon={MasterCard} cardType='Credit Card' />
+                <PriceText priceCount='$4444' />
             </Grid>
-            <Grid container>
+            <Grid container justifyContent='space-between' alignContent='center'>
                 <TransactionsTemp primaryText='Spar' ListIcon={<RestaurantIcon />} date='July 20, 2014' />
                 <CardsTemp primaryText='Spar' ListIcon={Visa3} cardType='Credit Card' />
+                <PriceText priceCount='$4444' />
             </Grid>
         </Box>
     )
