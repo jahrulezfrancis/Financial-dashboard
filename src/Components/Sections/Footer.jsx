69087ac2c938c650365e317a9c707dcc5da9edf1
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-03 11:59:07 +0100

    changed footer text

--- src/Components/Sections/Footer.jsx
+++ src/Components/Sections/Footer.jsx
@@ -1,9 +1,11 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
+import {useMediaQuery} from "@mui/material"
 
 export default function Footer() {
+    const onMobile = useMediaQuery('(max-width: 1000px)')
     return (
-        <Box>
-            
-        </Box>
+        <Box ml={onMobile ? '0em' : '5em'} >
+            <Typography color='#5B5B65'> © 2022 7thWeb Tech</Typography>
+        </ Box>
     )
 }
