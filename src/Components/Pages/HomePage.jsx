c6d4397abfb4d9953ca2f4101a1bb9270e726640
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-03 11:04:06 +0100

    added footer component

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/system";
 import HeaderIntro from "../Header/HeaderSection";
 import Navigation from "../Header/Navigation";
+import Footer from "../Sections/Footer";
 
 export function Homepage() {
     return (
         <Box sx={{bgcolor: 'black'}}>
             <Navigation />
             <HeaderIntro />
+            <Footer />
         </Box>
     )
 }
