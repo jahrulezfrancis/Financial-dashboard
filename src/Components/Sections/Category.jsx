a2c54bd48c07e4d7376457e25860e4f887575a6b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-03 08:04:24 +0100

    Initial commit

---
+++ src/Components/Sections/Category.jsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export function Categories() {
+    return (
+        <Box>
+            <Typography>Top categories</Typography>
+        </Box>
+    )
+}
