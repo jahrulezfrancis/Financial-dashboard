23b619080fe12e58f5855ba963a81fa43d8267d5
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-03 10:59:01 +0100

    changed the code structure

--- src/Components/Sections/Category.jsx
+++ src/Components/Sections/Category.jsx
@@ -1,9 +1,22 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
+import Category1 from "../Images/spend.png"
+import Category2 from "../Images/caschback.png"
+import Category3 from "../Images/spend-trend.png"
+import { Stack } from "@mui/system";
 
 export function Categories() {
     return (
-        <Box>
-            <Typography>Top categories</Typography>
-        </Box>
+        <Stack justifyContent='center' align='start' spacing={-2}>
+            <Box p='2em'>
+                <Typography pl='.8em' color='white' variant='h5'>Top categories</Typography>
+            </Box>
+            <Grid container gap={-3}>
+                    <img src={Category1} alt='spend trend' />
+                    <img src={Category2} alt='spend trend' />
+                <Box>
+                    <img src={Category3} alt='spend trend' />
+                </Box>
+            </Grid>
+        </Stack>
     )
 }
