9c8bb46bac276467dd3bca38e356f423f95bceea
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-03 20:53:16 +0100

    fixed responsiveness

--- src/Components/Sections/Category.jsx
+++ src/Components/Sections/Category.jsx
@@ -1,21 +1,20 @@
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography, Grid, useMediaQuery } from "@mui/material";
 import Category1 from "../Images/spend.png"
 import Category2 from "../Images/caschback.png"
 import Category3 from "../Images/spend-trend.png"
 import { Stack } from "@mui/system";
 
 export function Categories() {
+    const onMobile = useMediaQuery('(max-width: 1000px)')
     return (
-        <Stack justifyContent='center' align='start' spacing={-2}>
-            <Box p='2em'>
-                <Typography pl='.8em' color='white' variant='h5'>Top categories</Typography>
+        <Stack justifyContent='space-around' width='100%'spacing={1}>
+            <Box p='2em 1em'>
+                <Typography textAlign={onMobile ? 'center' : 'start'} color='white' variant='h5'>Top categories</Typography>
             </Box>
-            <Grid container gap={-3}>
+            <Grid container spacing={3} justifyContent='center'>
                     <img src={Category1} alt='spend trend' />
                     <img src={Category2} alt='spend trend' />
-                <Box>
                     <img src={Category3} alt='spend trend' />
-                </Box>
             </Grid>
         </Stack>
     )
