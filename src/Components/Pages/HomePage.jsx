1db533a3052ffe1f3ff35d9a6fe39f97275ccf54
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-10-04 13:23:05 +0100

    added mobile menu component

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/system";
 import HeaderIntro from "../Header/HeaderSection";
-import Navigation from "../Header/Navigation";
+import Navigation, { MobileMenu } from "../Header/Navigation";
 import Footer from "../Sections/Footer";
 
 export function Homepage() {
     return (
         <Box sx={{bgcolor: 'black'}}>
             <Navigation />
+            <MobileMenu />
             <HeaderIntro />
             <Footer />
         </Box>
